cc596b31bf0c195ca02cc37ba7f6eadd7a5dbcfd
aellison5505 <anthonyellison5505@gmail.com>
2019-01-07 18:03:38 -0500

    build update

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-cp lambda_func.py ./package
-cp lamda_bridge.py ./package
+cp lambda_func.py ./package/deploy
+cp lamda_bridge.py ./package/deploy
 aws s3 sync ./package s3://lambda-aladdin-con --delete
 aws codebuild start-build --project-name agc-backend --region us-east-1
 
